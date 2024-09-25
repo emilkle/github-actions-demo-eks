6b5e95f470571ed52f6b4e4a6ae10d49c29a9ba4
Emil <emilkle@stud.ntnu.no>
2024-09-25 22:21:24 +0200

    Added default values to variables

--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "rg_name" {
     type = string
     description = "(optional) describe your variable"
+    default = "rg-web-eks"
 }
 
 variable "sa_name" {
     type = string
     description = "(optional) describe your variable"
+    default = "sawebeks"
 }
 
 variable "location" {
     type = string
     description = "(optional) describe your variable"
+    default = "westeurope"
 }
 
 variable "source_content" {
     type = string
     description = "(optional) describe your variable"
+    default = "<h1>A web page created with Terraform</h1>"
 }
 
 variable "index_document" {
     type = string
     description = "(optional) describe your variable"
+    default = "index.html"
 }
